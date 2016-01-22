a4a09ef165cd5ba99fa404067dc6ae59a2ac3f1c
Julio C. Westarb Jr <jwestarb@gmail.com>
2016-01-21 22:58:01 -0200

    E-mail e Telefone opcionais

--- eventex/subscriptions/tests/test_view_new.py
+++ eventex/subscriptions/tests/test_view_new.py
@@ -78,3 +78,10 @@ class SubscriptionsNewPostInvalid(TestCase):
 
     def test_dont_save_subscription(self):
         self.assertFalse(Subscription.objects.exists())
+
+
+class TemplareRegressionTest(TestCase):
+    def test_template_has_non_field_errors(self):
+        invalid_data = dict(name='Julio Cesar Westarb Junior', cpf='12345678901')
+        response = self.client.post(r('subscriptions:new'), invalid_data)
+        self.assertContains(response, '<ul class="errorlist nonfield">')
